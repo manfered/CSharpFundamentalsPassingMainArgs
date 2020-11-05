24a7c76e87476a85f6fc5ea1ed068d0f8f63580d
manfered <farzad.seifi@gmail.com>
2020-11-04 22:01:38 -0700

    Add project files.

---
+++ CSharpFundamentalsPassingMainArgs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpFundamentalsPassingMainArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
